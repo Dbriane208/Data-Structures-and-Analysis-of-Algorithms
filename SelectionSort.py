0037f66f20164e9f531de0b5b1e9e6edd4d4988d
Daniel Brian <db9755949@gmail.com>
2023-06-21 18:58:30 +0300

    Selection sort

--- SelectionSort.py
+++ SelectionSort.py
@@ -8,16 +8,29 @@
 
 def Selection_sort(numValues):
 
-    for num in range(len(numValues)):
+    for num in range(len(numValues)-1):
         current_num = num
 
         for value in range(num+1, len(numValues)):
             if numValues[current_num] > numValues[value]:
                 current_num = value
+            
+        if num != current_num:
+                
+            numValues[num],numValues[current_num] = numValues[current_num],numValues[num]
 
-                numValues[num],numValues[current_num] = numValues[current_num],numValues[num]
+    return numValues
 
-list = [5, 2, 9, 1, 7]
-Selection_sort(list)
+list = [5, 1, 7, 2, 1, 9, 9, 7]
+print(Selection_sort(list))
 
-print(list)
+
+
+
+
+
+        
+    
+
+
+
