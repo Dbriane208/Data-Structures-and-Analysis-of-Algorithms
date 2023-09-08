d496ea2b72d07afa9e1436586827e6ba1c53f80e
Daniel Brian <db9755949@gmail.com>
2023-09-08 23:06:25 +0300

    Sorted_Squared Arary

--- SortedSquaredArray.py
+++ SortedSquaredArray.py
@@ -1,8 +1,8 @@
-#o(n) sapce and o(n) time complexity
+#o(n) space and o(n) time complexity
 
 def Sorted_Squared ():
 
-    array = [1,2,3,4,5]
+    array = [-5,-2,-1,0,1,2,3]
 
     sortedArray = []
 
@@ -16,3 +16,25 @@ def Sorted_Squared ():
 myArray = Sorted_Squared()
 
 print(myArray)
+
+#The code above is an improvement of the one below in time and space complexity
+#The one below has a time complexity of O(n2) and space complexity of O(n)
+
+# def squaredArray(list):
+    
+#     # //Perform a bubble sort then square the output
+#     for element in range(len(list)):
+#         for value in range(element + 1,len(list)):
+#             if abs(list[element]) > abs(list[value]):
+#                 temp = list[element]
+#                 list[element] = list[value]
+#                 list[value] = temp
+                
+#     for index in range(len(list)):
+#         list[index] *= list[index]
+        
+#     return list
+    
+# list = [-5,-2,-1,0,1,2,3]
+# sort = squaredArray(list)
+# print(sort)
