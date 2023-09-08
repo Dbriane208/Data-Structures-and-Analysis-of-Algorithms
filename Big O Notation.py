cd01687b9950cf071ecec6057d85dfc38ad15140
Daniel Brian <db9755949@gmail.com>
2023-09-08 23:16:27 +0300

    Big O notation

---
+++ Big O Notation.py
@@ -0,0 +1,7 @@
+#Constant time O(1)
+#The function takes aproximately same time to perform the same operation of given num input
+
+def example0(num):
+    return num % 2 == 0
+output = example0(5)
+print(output)
